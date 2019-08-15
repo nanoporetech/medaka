ad50c17a1c6e68ab43305c3d0ac51edb3b2addba
cwright <chris.wright@nanoporetech.com>
2019-08-15 15:23:56 +0100

    Add pystyle to test

--- medaka/__init__.py
+++ medaka/__init__.py
@@ -9,12 +9,13 @@ __version__ = '0.8.2'
 def check_minimap2_version():
     """get minimap2 version."""
     try:
-        proc = subprocess.run(["minimap2", "--version"], stdout=subprocess.PIPE)
+        proc = subprocess.run(
+            ["minimap2", "--version"], stdout=subprocess.PIPE)
         if proc.returncode != 0:
             return None
         # 2.11-r797
         version = LooseVersion(proc.stdout.decode().split('-')[0])
-    except:
+    except Exception as e:
         return None
 
     return version
@@ -29,30 +30,32 @@ def check_htslib_tool_version(tool, pos=2):
         first_line = proc.stdout.decode().split("\n", 1)[0]
         version = first_line.split()[pos]
         version = LooseVersion(version)
-    except:
+    except Exception as e:
         return None
 
     return version
 
-check_tabix_version = functools.partial(check_htslib_tool_version, 'tabix')
-check_bgzip_version = functools.partial(check_htslib_tool_version, 'bgzip')
-check_samtools_version = functools.partial(check_htslib_tool_version, 'samtools', 1)
 
+check_tabix_version = functools.partial(
+    check_htslib_tool_version, 'tabix')
+check_bgzip_version = functools.partial(
+    check_htslib_tool_version, 'bgzip')
+check_samtools_version = functools.partial(
+    check_htslib_tool_version, 'samtools', 1)
 
 required_version = {
-    'minimap2':LooseVersion('2.11'),
-    'samtools':LooseVersion('1.9'),
-    'tabix':LooseVersion('1.9'),
-    'bgzip':LooseVersion('1.9'),
+    'minimap2': LooseVersion('2.11'),
+    'samtools': LooseVersion('1.9'),
+    'tabix': LooseVersion('1.9'),
+    'bgzip': LooseVersion('1.9'),
 }
 
 get_version = {
-    'minimap2':check_minimap2_version,
-    'samtools':check_samtools_version,
-    'tabix':check_tabix_version,
-    'bgzip':check_bgzip_version,
+    'minimap2': check_minimap2_version,
+    'samtools': check_samtools_version,
+    'tabix': check_tabix_version,
+    'bgzip': check_bgzip_version,
 }
-
 
 
 def report_binaries():
@@ -61,7 +64,7 @@ def report_binaries():
 
     """
     fail = False
-    versions = {prog:get_version[prog]() for prog in required_version.keys()}
+    versions = {prog: get_version[prog]() for prog in required_version.keys()}
 
     width = 9
     cols = "Program Version Required Pass".split()
@@ -73,8 +76,10 @@ def report_binaries():
             versions[prog] = 'Not found'
         else:
             good = versions[prog] >= required_version[prog]
-        print('  '.join((str(x).ljust(width) for x in (prog, versions[prog], required_version[prog], good))))
+        print('  '.join((
+            str(x).ljust(width)
+            for x in (prog, versions[prog], required_version[prog], good)
+        )))
         all_good &= good
     if not all_good:
         os._exit(1)
-
