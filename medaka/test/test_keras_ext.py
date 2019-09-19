77a9f01e30dae7f8e3b9f19888d51b082821e750
Chris Wright <chris.wright@nanoporetech.com>
2019-09-19 17:41:58 +0100

    Manipulate tensorboard logs to display training and validation together

--- medaka/test/test_keras_ext.py
+++ medaka/test/test_keras_ext.py
@@ -1,4 +1,5 @@
 import functools
+import os
 import tempfile
 import unittest
 
@@ -30,7 +31,7 @@ def get_test_data(num_train=1000, num_test=500, input_shape=(10,),
     return (X[:num_train], y[:num_train]), (X[num_train:], y[num_train:])
 
 
-class ModelAndData(object):
+class ModelAndData(unittest.TestCase):
     input_dim = 2
     num_hidden = 4
     num_classes = 2
@@ -49,6 +50,33 @@ class ModelAndData(object):
 
     callback_opts = dict(verbose=0, save_best_only=True, mode='max')
     metrics = ['binary_accuracy']
+
+    @classmethod
+    def setUpClass(self):
+        (X_train, y_train), (X_test, y_test) = self._get_data_callbacks(self)
+        y_train = tf.keras.utils.to_categorical(y_train)
+        y_test = tf.keras.utils.to_categorical(y_test)
+
+        self.val_model_file = tempfile.NamedTemporaryFile()
+        self.val_model_fname = self.val_model_file.name
+        self.train_model_file = tempfile.NamedTemporaryFile()
+        self.train_model_fname = self.train_model_file.name
+        self.tb_dir = tempfile.mkdtemp()
+        callbacks = [
+            medaka.keras_ext.ModelMetaCheckpoint(
+                self.model_meta, self.val_model_fname,
+                monitor='val_{}'.format(self.metrics[0]),
+                **self.callback_opts),
+            medaka.keras_ext.ModelMetaCheckpoint(
+                self.model_meta, self.train_model_fname,
+                monitor=self.metrics[0],
+                **self.callback_opts),
+            medaka.keras_ext.TrainValTensorBoard(log_dir=self.tb_dir)
+        ]
+        model = self._get_model(self)
+        model.fit(
+            X_train, y_train, validation_data=(X_test, y_test),
+            batch_size=2, epochs=4, callbacks=callbacks)
 
     def _get_model(self):
         dense = tf.keras.layers.Dense
@@ -70,23 +98,10 @@ class ModelAndData(object):
             num_classes=self.num_classes)
 
 
-class TestCheckpoint(unittest.TestCase, ModelAndData):
+class TestCallbacks(ModelAndData):
 
     def test_000_checkpoint_saves_meta(self):
-        (X_train, y_train), (X_test, y_test) = self._get_data_callbacks()
-        y_train = tf.keras.utils.to_categorical(y_train)
-        y_test = tf.keras.utils.to_categorical(y_test)
-        model_file = tempfile.NamedTemporaryFile()
-        model_fname = model_file.name
-        callbacks = [
-            medaka.keras_ext.ModelMetaCheckpoint(
-                self.model_meta, model_fname, monitor='val_{}'.format(self.metrics[0]), **self.callback_opts)]
-        model = self._get_model()
-        model.fit(
-            X_train, y_train, validation_data=(X_test, y_test),
-            batch_size=2, epochs=5, callbacks=callbacks)
-
-        with medaka.datastore.DataStore(model_fname, 'r') as ds:
+        with medaka.datastore.DataStore(self.val_model_fname, 'r') as ds:
            model_func = ds.get_meta('model_function')
            # we can not simply assert equality of partial functions
            # https://bugs.python.org/issue3564
@@ -102,6 +117,18 @@ class TestCheckpoint(unittest.TestCase, ModelAndData):
         with self.assertRaises(KeyError):
             medaka.keras_ext.ModelMetaCheckpoint(meta, model_fname)
 
+    def test_010_tensorboard(self):
+        # check that both training and validation logs contain correctly names metrics
+        for group in ('training', 'validation'):
+            grp_path = os.path.join(self.tb_dir, group)
+            fname = [x for x in os.listdir(grp_path) if x != 'plugins' and not x.endswith('profile-empty')]
+            self.assertTrue(len(fname) == 1)
+            fname = fname[0]
+            found = False
+            with open(os.path.join(grp_path, fname), 'rb') as fh:
+                found = any(b'epoch_binary_accuracy' in line for line in fh)
+                self.assertTrue(found, "Failed to find correctly named metric for {}".format(group))
 
-class TestBatcher(unittest.TestCase, ModelAndData):
+
+class TestBatcher(ModelAndData):
     pass
