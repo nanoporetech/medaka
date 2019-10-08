9ba8e6a5d54fff8b500d41c7a2bb0110d396b7b6
jblasco <javier.blasco@nanoporetech.com>
2019-10-08 16:52:00 +0100

    Increase coverage of features.py

--- medaka/test/test_counts.py
+++ medaka/test/test_counts.py
@@ -10,12 +10,43 @@ import pysam
 import libmedaka
 import medaka.features
 from medaka.common import Region
+import medaka.labels
 
 __reads_bam__ = os.path.join(os.path.dirname(__file__), 'data', 'test_reads.bam')
+__reads_truth__ = os.path.join(os.path.dirname(__file__), 'data', 'truth_to_ref.bam')
 __two_type_bam__ = os.path.join(os.path.dirname(__file__), 'data', 'test_two_type.bam')
 __gapped_bam__ = os.path.join(os.path.dirname(__file__), 'data', 'reads_gapped.bam')
 __region__ = Region('Consensus_Consensus_Consensus_Consensus_utg000001l', start=50000, end=100000)
 __region_start__ = Region('Consensus_Consensus_Consensus_Consensus_utg000001l', start=0, end=200)
+
+
+def create_dtypes_tags_bam(fname):
+    """ Create a small bam file with 2 dtypes."""
+    bam_fname = tempfile.NamedTemporaryFile(suffix='.bam').name
+    create_rle_bam(bam_fname)
+
+    # Add r9/r10 to the reads in the file
+    with pysam.AlignmentFile(bam_fname) as input_bam:
+        tmp_file = '{}.tmp'.format(fname)
+        with pysam.AlignmentFile(tmp_file, 'wb', header=input_bam.header) as output_bam:
+            for ii, alignment in enumerate(input_bam):
+                # Add dtype tag
+                if ii % 2:
+                    alignment.set_tag('DT', 'r9')
+                else:
+                    alignment.set_tag('DT', 'r10')
+
+                # Add tag to some of the reads
+                if ii in (0, 1):
+                    alignment.set_tag('AA', 1)
+                elif ii == 2:
+                    alignment.set_tag('AA', 2)
+
+                output_bam.write(alignment)
+
+    pysam.sort("-o", fname, tmp_file)
+    os.remove(tmp_file)
+    pysam.index(fname)
 
 
 def create_rle_bam(fname):
@@ -25,8 +56,8 @@ def create_rle_bam(fname):
 
     Basecall1:   2A   1C   4A   5T        1G   1A   2T   1G
     Basecall2:   3A   1C   4A    *        1G   1A   1T   2G
-    Basecall3:   2A   1C   4A   5T   1A   1G   1A   2T   1G
-    Basecall4:   2A   1C   4A   1C        1G   1A   2T   1G
+    Basecall3:   2a   1c   4a   5t   1a   1g   1a   2t   1g
+    Basecall4:   2a   1c   4a   1c        1g   1a   2t   1g
 
     """
 
@@ -34,22 +65,26 @@ def create_rle_bam(fname):
     basecall1 = {
         'seq': 'ACATGATG',  # exactly ref
         'quality': array.array('B', [2, 1, 4, 5, 1, 1, 2, 1]),
-        'cigarstring': '8='}
+        'cigarstring': '8=',
+        'flag': 0}
 
     basecall2 = {
         'seq': 'ACAGATG',  # deletion of T in the middle
         'quality': array.array('B', [3, 1, 4, 1, 1, 1, 2]),
-        'cigarstring': '3=1D4='}
+        'cigarstring': '3=1D4=',
+        'flag': 0}
 
     basecall3 = {
         'seq': 'ACATAGATG',  # insertion of A in the middle
         'quality':  array.array('B', [2, 1, 4, 5, 1, 1, 1, 2, 1]),
-        'cigarstring': '4=1I4='}
+        'cigarstring': '4=1I4=',
+        'flag': 16}
 
     basecall4 = {
         'seq': 'ACACGATG',  # substitution T->C
         'quality': array.array('B', [2, 1, 4, 1, 1, 1, 2, 1]),
-        'cigarstring': '3=1X4='}
+        'cigarstring': '3=1X4=',
+        'flag': 16}
 
     header = {'HD': {'VN': '1.0'},
               'SQ': [{'LN': 8, 'SN': 'ref'}]}
@@ -67,7 +102,7 @@ def create_rle_bam(fname):
             a.query_sequence = basecall['seq']
             a.cigarstring = basecall['cigarstring']
 
-            a.flag = 0
+            a.flag = basecall['flag']
             a.mapping_quality = 50
             a.query_qualities = basecall['quality']
             output.write(a)
@@ -113,6 +148,23 @@ class CountsTest(unittest.TestCase):
         encoder = medaka.features.CountsFeatureEncoder(**kwargs)
         self.assertEqual(encoder.feature_vector_length, 20)
 
+    def test_030_bams_to_training_samples(self):
+        encoder = medaka.features.CountsFeatureEncoder(normalise='total')
+        label_scheme = medaka.labels.HaploidLabelScheme()
+        region = Region(
+            'Consensus_Consensus_Consensus_Consensus_utg000001l',
+            149744, 318288)
+        result = encoder.bams_to_training_samples(
+            __reads_truth__, __reads_bam__, region, label_scheme)[0]
+
+        expected_feature_shape = (161462, 10)
+        got_feature_shape = result.features.shape
+        self.assertEqual(expected_feature_shape, got_feature_shape)
+
+        expected_label_shape = (161462,)
+        got_label_shape = result.labels.shape
+        self.assertEqual(expected_label_shape, got_label_shape)
+
 
 class CountsSplittingTest(unittest.TestCase):
 
@@ -131,7 +183,6 @@ class CountsSplittingTest(unittest.TestCase):
             for i in (0, 1):
                 # check both pileup and positions
                 self.assertEqual(exp_len, len(chunk[i]))
-
 
 
 class SampleGenerator(unittest.TestCase):
@@ -164,9 +215,83 @@ class SampleGenerator(unittest.TestCase):
         samples = sample_gen.samples
         self.assertEqual(len(sample_gen._quarantined), 1)
 
+    def test_020_mandatory_label_scheme(self):
+        """A SampleGenerator initialised with truth_bam needs a label_scheme"""
+        encoder = medaka.features.CountsFeatureEncoder()
+        with self.assertRaises(ValueError) as context:
+            sample_gen = medaka.features.SampleGenerator(
+                __reads_bam__, __region__, encoder, truth_bam=__reads_truth__)
 
 
 
+class PileupCounts(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        bam_fname = tempfile.NamedTemporaryFile(suffix='.bam').name
+        create_dtypes_tags_bam(bam_fname)
+        cls.region = Region(
+            'ref',
+            start=0, end=8)
+        cls.bam = bam_fname
+
+    def test_000_counts_no_dtypes(self):
+        counts, positions = medaka.features.pileup_counts(
+            self.region, self.bam)[0]
+
+        expected_counts = np.array(
+            [[2, 0, 0, 0, 2, 0, 0, 0, 0, 0],
+             [0, 2, 0, 0, 0, 2, 0, 0, 0, 0],
+             [2, 0, 0, 0, 2, 0, 0, 0, 0, 0],
+             [0, 1, 0, 1, 0, 0, 0, 1, 0, 1],
+             [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 2, 0, 0, 0, 2, 0, 0, 0],
+             [2, 0, 0, 0, 2, 0, 0, 0, 0, 0],
+             [0, 0, 0, 2, 0, 0, 0, 2, 0, 0],
+             [0, 0, 2, 0, 0, 0, 2, 0, 0, 0]], dtype=np.uint64)
+        self.assertTrue(np.array_equal(counts, expected_counts))
+
+        expected_positions = np.array(
+            [(0, 0), (1, 0), (2, 0), (3, 0), (3, 1),
+             (4, 0), (5, 0), (6, 0), (7, 0)],
+             dtype=[('major', '<i8'), ('minor', '<i8')])
+        self.assertTrue(np.array_equal(positions, expected_positions))
+
+    def test_010_tags(self):
+        """Only two reads have tag AA=1."""
+        counts, positions = medaka.features.pileup_counts(
+            self.region, self.bam, tag_name='AA', tag_value=1,
+            keep_missing=False)[0]
+        expected_number_reads = {2}
+        self.assertEqual(expected_number_reads, set(counts.sum(axis=1)))
+
+    def test_020_tags(self):
+        """If keep_missing is True, 3 reads are expected"""
+        counts, positions = medaka.features.pileup_counts(
+            self.region, self.bam, tag_name='AA', tag_value=1,
+            keep_missing=True)[0]
+        expected_number_reads = {3}
+        self.assertEqual(expected_number_reads, set(counts.sum(axis=1)))
+
+
+    def test_030_tag_error(self):
+        """Tags should be two letters"""
+        with self.assertRaises(ValueError) as context:
+            counts, positions = medaka.features.pileup_counts(
+                self.region, self.bam, tag_name='AAC', tag_value=1,
+                keep_missing=True)[0]
+
+        with self.assertRaises(ValueError) as context:
+            counts, positions = medaka.features.pileup_counts(
+                self.region, self.bam, tag_name='A', tag_value=1,
+                keep_missing=True)[0]
+
+    def test_040_dtypes(self):
+        counts, positions = medaka.features.pileup_counts(
+            self.region, self.bam, dtype_prefixes=['r9', 'r10'])[0]
+
+        expected_shape = (9, 20)
+        self.assertEqual(expected_shape, counts.shape)
 
 
 class CountsQscoreStratification(unittest.TestCase):
@@ -254,7 +379,8 @@ class HardRLEFeatureEncoder(unittest.TestCase):
         # Create a bam file where we know the alignments
         RLE_bam = tempfile.NamedTemporaryFile(suffix='.bam').name
         create_rle_bam(RLE_bam)
-        sample = encoder.bam_to_sample(RLE_bam, Region('ref', 0, 8))
+        sample = encoder.bam_to_sample(RLE_bam, Region('ref', 0, 11))
+        cls.bam_fname = RLE_bam
         cls.sample = sample[0]
         cls.num_qstrat = encoder.num_qstrat
 
@@ -276,7 +402,7 @@ class HardRLEFeatureEncoder(unittest.TestCase):
         self.assertEqual(self.sample.features.shape, (9, featlen * self.num_qstrat))
 
     def test_004_check_counts(self):
-        """Check the counts themselves. See above create_rle_bam to understand
+        """Check the counts themselves. See above `create_rle_bam` to understand
          the values in the counts. For example, 3 basecalls believe the first
          column of the alignment is `2A`, while one has `3A`. Thus, index
          [0, 14] should contain 3 and [0, 24] should show a 1.
@@ -284,16 +410,37 @@ class HardRLEFeatureEncoder(unittest.TestCase):
 
         # dictionary with value: index encoding
         values_for_positions = {
-            1: ([0, 24], [3, 9], [3, 5], [4, 4], [7, 7], [8, 16]),
-            2: ([3, 47],),
-            3: ([0, 14], [7, 17], [8, 6]),
-            4: ([1, 5], [2, 34], [5, 6], [6, 4])}
+            1: ([0, 14], [0, 24], [3, 1], [3, 9], [3, 43], [3, 47],
+                [4, 0], [7, 7], [7, 17], [8, 6], [8, 16]),
+            2: ([0, 10], [1, 1], [1, 5], [2, 30], [2, 34], [5, 2],
+                [5, 6], [6, 0], [6, 4], [7, 13], [8, 2])}
         expected = np.zeros_like(self.sample.features)
         for value, positions in values_for_positions.items():
             for pos in positions:
                 expected[pos[0], pos[1]] = value
 
-        self.assertSequenceEqual(self.sample.features.tolist(), expected.tolist())
+        self.assertSequenceEqual(
+            self.sample.features.tolist(), expected.tolist())
+
+    def test_005_check_fwd_rev(self):
+        """Split normalisation between fwd and rev reads. """
+        encoder = medaka.features.HardRLEFeatureEncoder(normalise='fwd_rev')
+        region = Region('ref', 0, 11)
+        sample = encoder.bam_to_sample(self.bam_fname, region)[0]
+        values_for_positions = {
+            0.5: ([0, 14], [0, 24], [3, 1], [3, 9], [3, 43], [3, 47],
+                  [4, 0], [7, 7], [7, 17], [8, 6], [8, 16]),
+            1: ([0, 10], [1, 1], [1, 5], [2, 30], [2, 34], [5, 2],
+                [5, 6], [6, 0], [6, 4], [7, 13],  [8, 2])}
+        expected = np.zeros_like(sample.features)
+        for value, positions in values_for_positions.items():
+            for pos in positions:
+                expected[pos[0], pos[1]] = value
+
+        self.assertSequenceEqual(
+            sample.features.tolist(), expected.tolist())
+
+
 
     def test_020_feature_length(self):
         # hardcode value here, if this genuinely needs to change at least
@@ -303,6 +450,3 @@ class HardRLEFeatureEncoder(unittest.TestCase):
         encoder = medaka.features.HardRLEFeatureEncoder(
             num_qstrat=10, **kwargs)
         self.assertEqual(encoder.feature_vector_length, 200)
-
-
-
