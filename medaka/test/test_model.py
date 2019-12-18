892e23daafb0eb9442f462d5eb8ae7c2024fed8e
Chris Wright <chris.wright@nanoporetech.com>
2019-12-18 11:11:33 +0000

    Check model meta in tests

--- medaka/test/test_model.py
+++ medaka/test/test_model.py
@@ -4,9 +4,12 @@ import unittest
 import numpy as np
 import tensorflow
 
+from medaka import models
 from medaka.medaka import model_dict
 from medaka.common import Sample
-from medaka import models
+from medaka.datastore import DataStore
+from medaka.features import BaseFeatureEncoder
+from medaka.labels import BaseLabelScheme
 
 
 class TestModels(unittest.TestCase):
@@ -15,6 +18,13 @@ class TestModels(unittest.TestCase):
         for name, model_file in model_dict.items():
             model = models.load_model(model_file)
             self.assertIsInstance(model, tensorflow.keras.models.Model)
+            # Check we can get necessary functions for inference
+            with DataStore(model_file) as ds:
+                feature_encoder = ds.get_meta('feature_encoder')
+                self.assertIsInstance(feature_encoder, BaseFeatureEncoder)
+                label_scheme = ds.get_meta('label_scheme')
+                self.assertIsInstance(label_scheme, BaseLabelScheme)
+
 
 class TestMajorityModel(unittest.TestCase):
 
