aa90b0955c83b218b6897ac49d51d4dde23df4ff
Chris Wright <chris.wright@nanoporetech.com>
2020-01-24 17:33:45 +0000

    Parasail

--- build-wheels.sh
+++ build-wheels.sh
@@ -4,6 +4,7 @@ set -e -x
 export MANYLINUX=1
 
 PACKAGE_NAME='medaka'
+parasail=parasail-1.1.17-py2.py3-none-linux_x86_64.whl
 
 workdir=$1
 shift
@@ -14,13 +15,16 @@ cd ${workdir}
 yum install -y zlib-devel bzip2 bzip2-devel xz-devel curl-devel openssl-devel ncurses-devel
 rm -rf libhts.a bincache/*
 make scripts/mini_align clean libhts.a
-
+mkdir -p wheelhouse
+curl -L https://ont-research.s3-eu-west-1.amazonaws.com/${parasail} \
+    -o ${parasail}
 
 # Compile wheels
 for minor in $@; do
     PYBIN="/opt/python/cp3${minor}-cp3${minor}m/bin"
     # auditwheel/issues/102
     "${PYBIN}/pip" install --upgrade cffi setuptools pip wheel==0.31.1
+    "${PYBIN}/pip" install ${parasail}
     "${PYBIN}/pip" wheel . -w ./wheelhouse/
 done
 
@@ -34,8 +38,9 @@ done
 # Install packages
 for minor in $@; do
     PYBIN="/opt/python/cp3${minor}-cp3${minor}m/bin"
+    "${PYBIN}/pip" install ${parasail}
     "${PYBIN}/pip" install "${PACKAGE_NAME}" --no-index -f ./wheelhouse
-    "${PYBIN}/medaka_counts" --print medaka/test/data/test_reads.bam Consensus_Consensus_Consensus_Consensus_utg000001l:10000-10010
+    "${PYBIN}/medaka_counts" --print medaka/test/data/test_reads.bam utg000001l:10000-10010
 done
 
 cd wheelhouse && ls | grep -v "${PACKAGE_NAME}.*manylinux" | xargs rm
