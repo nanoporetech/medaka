ba5ddd153363d666614403a9fdd08133d6dd966b
Javier Blasco <javier.blasco@nanoporetech.com>
2020-04-16 16:35:37 +0100

    Implement blocked RLE

--- src/fastrle.h
+++ src/fastrle.h
@@ -1,7 +1,12 @@
 #ifndef _MEDAKA_FASTRLE_H
 #define _MEDAKA_FASTRLE_H
 
-// Read a fasta/q and writes quality-RLE fastq file
-size_t fastrle(char *fname);
-
+/**  Read a fasta/q and writes quality-RLE fastq file.
+ *
+ *  @param fname name of file to be rle-compressed.
+ *  @param block_size maximum RLE compressed size, homopolymers larger than this value will be split in blocks.
+ *  @returns 
+ *
+ */
+size_t fastrle(char *fname, size_t block_size);
 #endif
